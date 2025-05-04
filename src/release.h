13138d9ffca9b204ac89c4799506530faaf20279
wonyongg <111210881+wonyongg@users.noreply.github.com>
2025-05-04 23:53:14 +0900

    first compile

---
+++ src/release.h
@@ -0,0 +1,5 @@
+#define REDIS_GIT_SHA1 "5af9fc25"
+#define REDIS_GIT_DIRTY "       0"
+#define REDIS_BUILD_ID "Wonyongui-MacBookAir.local-1746357926"
+#include "version.h"
+#define REDIS_BUILD_ID_RAW SERVER_NAME VALKEY_VERSION REDIS_BUILD_ID REDIS_GIT_DIRTY REDIS_GIT_SHA1
