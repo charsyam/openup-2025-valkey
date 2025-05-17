a80c1505359adb4cfe122b35b825e848d74e9859
punchdrunkard <o_o___t@pusan.ac.kr>
2025-05-17 17:52:58 +0900

    feat: 커스텀 echo 명령어 `echoPunchDrunkard` 추가

--- src/server.c
+++ src/server.c
@@ -4697,6 +4697,11 @@ void echoCommand(client *c) {
     addReplyBulk(c, c->argv[1]);
 }
 
+void echoCommandPunchDrunkard(client *c) {
+    addReplyBulk(c, c->argv[1]);
+}
+
+
 void timeCommand(client *c) {
     addReplyArrayLen(c, 2);
     addReplyBulkLongLong(c, server.unixtime);
